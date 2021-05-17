51eb9507e5bd766d24ba85e864d694f81bf9f4b2
anjan2052 <anjan2052@gmail.com>
2021-05-17 20:36:06 +0545

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import './body.dart';
 import './const.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,26 +18,207 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainScaffold extends StatelessWidget {
-  const MainScaffold({
-    Key key,
-  }) : super(key: key);
+class MainScaffold extends StatefulWidget {
+  @override
+  _MainScaffoldState createState() => _MainScaffoldState();
+}
 
+class _MainScaffoldState extends State<MainScaffold> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
-        leading: IconButton(
-          icon: Icon(
-            Icons.menu,
-          ),
-          onPressed: () {},
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: primarycolor,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  (Text(
+                    "Weather App",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  )),
+                ],
+              ),
+            )
+          ],
         ),
-        // title: Text("Weather App"),
-        centerTitle: true,
       ),
       body: Body(),
+    );
+  }
+}
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            height: size.height * 0.15,
+            child: Stack(
+              children: [
+                Container(
+                  height: size.height * 0.15 - 20,
+                  decoration: BoxDecoration(
+                      color: primarycolor,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(36),
+                        bottomRight: Radius.circular(36),
+                      )),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.symmetric(horizontal: 20),
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                            offset: Offset(0, 10),
+                            blurRadius: 50,
+                            color: primarycolor.withOpacity(0.3))
+                      ],
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        hintStyle: TextStyle(
+                          color: primarycolor.withOpacity(0.5),
+                        ),
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        suffixIcon: Icon(Icons.search),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Stack(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                height: size.height * 0.26,
+                decoration: BoxDecoration(
+                  color: primarycolor.withOpacity(0.9),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              Positioned(
+                child: Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  child: (Column(
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Today's Weather",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.place,
+                            color: Colors.white,
+                          ),
+                          SizedBox(width: 15),
+                          Text(
+                            "Kathmandu",
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                "30\u00B0C",
+                                style: TextStyle(
+                                    fontSize: 35, color: Colors.white),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                "Feels like 31\u00B0C",
+                                style: TextStyle(
+                                    color: Colors.white.withOpacity(0.8)),
+                              )
+                            ],
+                          ),
+                          SizedBox(width: 80),
+                          Column(
+                            children: [
+                              SvgPicture.asset(
+                                "assets/svg/09n.svg",
+                                height: 60,
+                                width: 60,
+                                color: Colors.white,
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                "Rainy",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  )),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
